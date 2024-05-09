ee26241cc880c2d08e7c354c759bc536197e7ef0
yongja <kyj00001@gmail.com>
2024-05-09 21:10:28 +0900

    change comments

--- scrapper_hotdeal/scrapper_hotdeal/pipelines.py
+++ scrapper_hotdeal/scrapper_hotdeal/pipelines.py
@@ -15,9 +15,8 @@ class ScrapperHotdealPipeline:
     def process_item(self, item, spider):
         return item
 
-class PreprocessingPipeline:
+class PreprocessingPipeline:    # 데이터 전처리
     def process_item(self, item, spider):
-        # 각 필드에 대한 전처리 수행
         item['title'] = self.clean_title(item['title'])
         item['register_time'] = self.clean_register_time(item['register_time'])
 
@@ -36,17 +35,17 @@ class PreprocessingPipeline:
         yesterday_date = now - timedelta(days=1)
         yesterday_date_str = yesterday_date.strftime('%Y-%m-%d') 
         if int(cleaned_register_time[:2]) > int(current_hour):
-            cleaned_register_time = datetime.strptime((current_date + ' ' + cleaned_register_time), '%Y-%m-%d %H:%M')
+            cleaned_register_time = datetime.strptime((yesterday_date_str + ' ' + cleaned_register_time), '%Y-%m-%d %H:%M')
         elif int(cleaned_register_time[:2]) < int(current_hour):
-            cleaned_register_time = datetime.strptime((yesterday_date_str + ' ' + cleaned_register_time), '%Y-%m-%d %H:%M')
-        elif int(cleaned_register_time[:2]) == int(current_hour) and int(cleaned_register_time[-2:]) < int(current_minute):
-            cleaned_register_time = datetime.strptime((yesterday_date_str + ' ' + cleaned_register_time), '%Y-%m-%d %H:%M')
-        else:
             cleaned_register_time = datetime.strptime((current_date + ' ' + cleaned_register_time), '%Y-%m-%d %H:%M')
+        elif int(cleaned_register_time[:2]) == int(current_hour) and int(cleaned_register_time[-2:]) < int(current_minute):
+            cleaned_register_time = datetime.strptime((current_date + ' ' + cleaned_register_time), '%Y-%m-%d %H:%M')
+        else:
+            cleaned_register_time = datetime.strptime((yesterday_date_str + ' ' + cleaned_register_time), '%Y-%m-%d %H:%M')
         return cleaned_register_time
-## 현재 시간이 15시인데 등록 시간이 17:xx이다? 이러면 어제 날짜 넣rl. 이거 이전이면(0시~14시) -> 오늘 날짜.    
 
-class SaveToDatabasePipeline:
+
+class SaveToDatabasePipeline:   # 데이터 저장
     @sync_to_async
     def process_item(self, item, spider):
         title = item.get('title')
@@ -55,8 +54,7 @@ class SaveToDatabasePipeline:
         info = item.get('info')
         url = item.get('url')
 
-        # 데이터 저장
-        if not ScrappingModel.objects.filter(url=url).exists():
+        if not ScrappingModel.objects.filter(url=url).exists(): # url을 id로 사용해서 중복 시 저장안되게
             ScrappingModel.objects.create(
                 title=title,
                 category=category,
@@ -69,7 +67,3 @@ class SaveToDatabasePipeline:
 
         return item
  
-
-# 날짜 로직
-# 중복 처리? 이건 걍 앞에서 url로 처리해보자
-## 그리고 register_time의 글자가 몇 글자 이상이면 뭐 종료 이런식으로 페이지 넘어가서 스크래핑하게 만들자(\t때문에 len은 7이 됨. 8 이상이면 종료)
