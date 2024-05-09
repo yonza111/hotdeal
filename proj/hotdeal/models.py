611fd7e6e6a845c8d3781fdce5c7c0ba552b4d22
yongja <kyj00001@gmail.com>
2024-05-09 21:39:25 +0900

    modify model(add active)

--- proj/hotdeal/models.py
+++ proj/hotdeal/models.py
@@ -10,6 +10,7 @@ class ScrappingModel(models.Model):
     delivery_fee = models.CharField(max_length=20)
     register_time = models.DateTimeField(default=timezone.now)
     url = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.title}_{self.price}_{self.category}{self.register_time}"
