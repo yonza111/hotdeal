896ff66786c43456f0e5d018f1b70397e6ff4c00
yongja <kyj00001@gmail.com>
2024-05-12 15:49:07 +0900

    add ScrappingModelAdmin

--- proj/hotdeal/admin.py
+++ proj/hotdeal/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import ScrappingModel
 # Register your models here.
 
-admin.site.register(ScrappingModel)
+def deactivate(modeladmin, request, queryset):
+    queryset.update(active=False)
+deactivate.short_description = "해당 데이터 비활성화"
+
+class ScrappingModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'register_time', 'active']
+    list_display_links = ['title']
+    list_filter = ['category']
+    search_fields = ['title']
+    actions = [deactivate]
+admin.site.register(ScrappingModel, ScrappingModelAdmin)
