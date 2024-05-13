f31fa0442d331310557458c6319483c6171d84f8
yongja <kyj00001@gmail.com>
2024-05-13 12:24:24 +0900

    list, detail view

--- proj/hotdeal/urls.py
+++ proj/hotdeal/urls.py
@@ -1,12 +1,10 @@
 from django.urls import include, path
-from rest_framework import routers
 from .views import ScrappingModelViewSet
+
 
 app_name = "hotdeal"
 
-router = routers.DefaultRouter()
-router.register(r"list", ScrappingModelViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
-]
+     path('scrapping-list/', ScrappingModelViewSet.as_view({'get': 'list'}), name='scrapping_list'),
+    ]
--- proj/hotdeal/views.py
+++ proj/hotdeal/views.py
@@ -1,7 +1,22 @@
 from rest_framework import viewsets
+from django.shortcuts import render
 from .models import ScrappingModel
 from .serializers import ScrappingModelSerializer
 
 class ScrappingModelViewSet(viewsets.ModelViewSet):
     queryset = ScrappingModel.objects.all()
     serializer_class = ScrappingModelSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        context = {
+            'scrappings': serializer.data
+        }
+        return render(request, 'scrapping_list.html', context)
+
+from django.shortcuts import render, get_object_or_404
+
+def scrapping_detail_view(request, scrapping_id):
+    scrapping = get_object_or_404(ScrappingModel, pk=scrapping_id)
+    return render(request, 'scrapping_detail.html', {'scrapping':scrapping})
