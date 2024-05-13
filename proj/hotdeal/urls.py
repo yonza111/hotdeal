4e871d9bb5004c2d71b204d16acaed628a49653a
yongja <kyj00001@gmail.com>
2024-05-13 20:40:28 +0900

    draft of detail view, list view

--- proj/hotdeal/urls.py
+++ proj/hotdeal/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from .views import ScrappingModelViewSet
-
+from .views import ScrappingModelListView, board, detail
 
 app_name = "hotdeal"
 
 
 urlpatterns = [
-     path('scrapping-list/', ScrappingModelViewSet.as_view({'get': 'list'}), name='scrapping_list'),
-    ]
+    path('api/scrapping-list/', ScrappingModelListView.as_view(), name='scrapping-list'),
+    path('board/', board, name='board'),
+    path('board/<int:pk>/', detail, name='detail'),
+     ]
--- proj/hotdeal/views.py
+++ proj/hotdeal/views.py
@@ -1,22 +1,22 @@
-from rest_framework import viewsets
-from django.shortcuts import render
+from rest_framework import generics
 from .models import ScrappingModel
 from .serializers import ScrappingModelSerializer
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
-class ScrappingModelViewSet(viewsets.ModelViewSet):
+
+class ScrappingModelListView(generics.ListAPIView):
     queryset = ScrappingModel.objects.all()
     serializer_class = ScrappingModelSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        context = {
-            'scrappings': serializer.data
-        }
-        return render(request, 'scrapping_list.html', context)
+def board(request):
+    all_hotdeals = ScrappingModel.objects.all()
+    posts_per_page = 20
+    paginator = Paginator(all_hotdeals, posts_per_page)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'scrapping_list.html', {'page_obj': page_obj})
 
-from django.shortcuts import render, get_object_or_404
-
-def scrapping_detail_view(request, scrapping_id):
-    scrapping = get_object_or_404(ScrappingModel, pk=scrapping_id)
-    return render(request, 'scrapping_detail.html', {'scrapping':scrapping})
+def detail(request, pk):
+    post = get_object_or_404(ScrappingModel, pk=pk)
+    return render(request, 'scrapping_detail.html', {'post': post})
