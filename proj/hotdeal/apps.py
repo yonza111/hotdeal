282a04a6833183334f00d5d6e5cef099e6807f4c
yongja <kyj00001@gmail.com>
2024-05-19 14:27:24 +0900

    schedulers(now deactivated)

--- proj/hotdeal/apps.py
+++ proj/hotdeal/apps.py
@@ -1,6 +1,36 @@
 from django.apps import AppConfig
+from django.conf import settings
+import logging
 
+logger = logging.getLogger(__name__)
 
 class HotdealConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hotdeal'
+    # initialized = False  # 초기화 여부를 확인하는 플래그
+
+    # def ready(self):
+    #     if not self.initialized:  # 초기화되지 않았을 때만 실행
+    #         if settings.SCHEDULER_DEFAULT:
+    #             from .tasks import start
+    #             start()
+    #             logger.info("Scheduler started")
+    #         self.initialized = True  # 초기화 완료 후 플래그 설정
+
+
+# from django.apps import AppConfig
+# from django.conf import settings
+
+# class HotdealConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'hotdeal'
+
+#     def ready(self):
+#         if settings.SCHEDULER_DEFAULT:
+#             from . import tasks
+#             tasks.start()
+#             print("start")
+
+#             # scrapy project를 장고 proj 안에 넣어보자
+
+
--- proj/hotdeal/tasks.py
+++ proj/hotdeal/tasks.py
@@ -1,25 +1,56 @@
-# scrapy 주기적 실행과 db에 일정 시간 지난 데이터들 비활성화 하는 2개 함수 만들기.
-
+import logging
 from apscheduler.schedulers.background import BackgroundScheduler 
-from scrapper_hotdeal.crawler import run_scrapy_crawler
 from datetime import timedelta
 from django.utils import timezone
 from .models import ScrappingModel
 
-scheduler = BackgroundScheduler()  # 백그라운드로 실행해야 함.
+logger = logging.getLogger(__name__)
+
+  # 백그라운드로 실행해야 함.
 
 def deactivate_olddata():
     threshold_date = timezone.now() - timedelta(hours=48)
-    ScrappingModel.objects.filter(created_at__lte=threshold_date).update(active=False) # 비활성화
+    logger.info(f"Deactivating data older than: {threshold_date}")
+    old_data_count = ScrappingModel.objects.filter(register_time__lte=threshold_date).count() # 비활성화
+    logger.info(f"Found {old_data_count} records to deactivate.")
+
+    if old_data_count > 0:
+        ScrappingModel.objects.filter(register_time__lte=threshold_date).update(active=False) # 비활성화
+        logger.info("Old data has been deactivated.")  # 작업이 실행되었을 때 로그 메시지 추가
+    else:
+        logger.info("No old data to deactivate.")
 
 
-# 스케줄링 작업 등록
-scheduler.add_job(run_scrapy_crawler, 'interval', hours=3)
-scheduler.add_job(deactivate_olddata, 'cron', hour=23, minute=50)
+import subprocess
+
+def run_scrapy_crawler():
+    # Scrapy 프로젝트 디렉토리로 이동
+    scrapy_project_path = 'scrapper_hotdeal'  # Scrapy 프로젝트의 실제 경로로 수정
+    scrapy_command = 'scrapy crawl hotdeal'  # 실행할 Scrapy 명령어
+
+    # subprocess를 사용하여 Scrapy 명령어 실행
+    try:
+        logger.info("Crawling started.")  # 작업이 실행되기 전에 로그 메시지 추가
+        subprocess.run(scrapy_command, shell=True, cwd=scrapy_project_path)
+        logger.info("Crawling completed.")  # 작업이 완료된 후에 로그 메시지 추가
+        deactivate_olddata()  # 크롤링이 끝난 후 데이터 비활성화 작업 실행
+    except Exception as e:
+        # 실행 중 오류가 발생한 경우 처리
+        print(f"Error running Scrapy crawler: {e}")
 
 
 # 스케줄링 작업 실행
-try:
-    scheduler.start()
-except KeyboardInterrupt:
-    scheduler.shutdown()
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(run_scrapy_crawler, 'interval', minutes=1)
+    # scheduler.add_job(deactivate_olddata, 'interval', minutes=1)
+    # scheduler.add_job(run_scrapy_crawler, 'interval', hours=1)
+    # scheduler.add_job(deactivate_olddata, 'interval', hours=1)
+    try:
+      logger.info("Starting scheduler...")
+      scheduler.start() # 없으면 동작하지 않습니다.
+    except KeyboardInterrupt:
+      logger.info("Stopping scheduler...")
+      scheduler.shutdown()
+      logger.info("Scheduler shut down successfully!")
+
