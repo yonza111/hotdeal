eff06bca9b7513ec24875d442df504a5a3ef3f9d
yongja <kyj00001@gmail.com>
2024-05-20 16:15:25 +0900

    model change to modify duplicate prevent logic

--- proj/keyword_manager/models.py
+++ proj/keyword_manager/models.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import User
 
 class Keyword(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=100, unique=True)
+    text = models.CharField(max_length=100)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'text'], 
+                                    name='unique_user_keyword')
+        ]
+        # unique=True 하면 user과 관계없이 text 한번 등록되면 끝이라서
+        # UniqueConstraint로 user-text 두 필드의 조합이 
+        # 데이터베이스 내에서 유일해야 함을 의미
     def __str__(self):
         return self.text
