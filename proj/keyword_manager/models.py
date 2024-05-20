cbd08a4d78e34a31db61fe22eaac158df0e3fcb4
yongja <kyj00001@gmail.com>
2024-05-20 15:37:23 +0900

    add unique option in keyword.text

--- proj/keyword_manager/models.py
+++ proj/keyword_manager/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Keyword(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=100)
+    text = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.text
