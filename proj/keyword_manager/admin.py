c7af68c20447b9d5d352e15583a5bccf43e5e44f
yongja <kyj00001@gmail.com>
2024-05-20 20:28:19 +0900

    make DiscordMessage model to send dm

--- proj/keyword_manager/admin.py
+++ proj/keyword_manager/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Keyword
+from .models import Keyword, DiscordMessage
 
 class KeywordAdmin(admin.ModelAdmin):
     list_display = ['user', 'text']
     list_display_links = ['user']
 admin.site.register(Keyword, KeywordAdmin)
+
+
+class DiscordMessageAdmin(admin.ModelAdmin):
+    list_display = ['user', 'discord_uid', 'active', 'keyword']
+    list_display_links = ['user']
+admin.site.register(DiscordMessage, DiscordMessageAdmin)
--- proj/keyword_manager/models.py
+++ proj/keyword_manager/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from allauth.socialaccount.models import SocialAccount
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 
 class Keyword(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,3 +19,37 @@ class Keyword(models.Model):
         # 데이터베이스 내에서 유일해야 함을 의미
     def __str__(self):
         return self.text
+    
+
+
+class DiscordMessage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    keyword = models.ForeignKey(Keyword, on_delete=models.CASCADE)
+    discord_uid = models.CharField(max_length=50, null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Discord Message"
+
+    @classmethod
+    def create_discord_uid(cls, user):
+        user_social_account = SocialAccount.objects.filter(user_id=user.id, provider='discord').first()
+        if user_social_account:
+            return user_social_account.uid
+        return None
+    
+    @classmethod
+    def get_user_keywords(cls, user):
+        return Keyword.objects.filter(user=user)
+    
+
+@receiver(post_save, sender=Keyword)
+def create_discord_message(sender, instance, created, **kwargs):
+    if created:
+        user = instance.user
+        discord_uid = DiscordMessage.create_discord_uid(user)
+        DiscordMessage.objects.create(user=user, keyword=instance, discord_uid=discord_uid)
+
+@receiver(post_delete, sender=Keyword)
+def delete_discord_message(sender, instance, **kwargs):
+    DiscordMessage.objects.filter(keyword=instance).delete()
