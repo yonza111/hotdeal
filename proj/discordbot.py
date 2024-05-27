95f8a442d4bc214abcb69e53134aa1e3367cd2c8
yongja <kyj00001@gmail.com>
2024-05-27 14:25:09 +0900

    discord embed message

--- proj/discordbot.py
+++ proj/discordbot.py
@@ -23,7 +23,7 @@ async def loop_message():
     while True:
         now = datetime.datetime.now()
         logging.info(f"{now} for loop_message")
-        if now.minute == 10:
+        if now.minute == 24:
             logging.info("send_hotdeal_alerts 실행 시작")
             await bot.send_hotdeal_alerts()
         await asyncio.sleep(60)
@@ -55,7 +55,7 @@ class HotdealBot(discord.Client):
 
         for user, user_data in user_messages.items():  # 그룹화된 유저별 메시지들을 순회
             keywords = user_data['keywords']
-            messages = []  # 유저별로 보낼 메시지들을 저장할 리스트
+            embeds = []  # 유저별로 보낼 임베드 메시지를 저장할 리스트
 
             for keyword in keywords:  # 각 유저의 키워드들을 순회
                 # 키워드에 해당하는 핫딜들을 가져옴
@@ -63,22 +63,76 @@ class HotdealBot(discord.Client):
                     ScrappingModel.objects.filter(title__icontains=keyword, active=True)
                 )
                 if hotdeals:  # 핫딜이 있는 경우에만
-                    keyword_message = f"'{keyword}' 검색 결과:\n" + "\n".join(
-                        [f"{hotdeal.title}: {hotdeal.url}" for hotdeal in hotdeals]
+                    embed = discord.Embed(
+                        title=f"'{keyword}' 검색 결과",
+                        description="",
+                        color=discord.Color.blue()
                     )
-                    messages.append(keyword_message)  # 메시지 리스트에 추가
+                    for hotdeal in hotdeals:
+                        embed.add_field(
+                            name=f"({hotdeal.title}    {hotdeal.price})",
+                            value=hotdeal.url,
+                            inline=False
+                        )
+                    embeds.append(embed)  # 임베드 리스트에 추가
 
-            if messages:  # 메시지가 있는 경우에만
+            if embeds:  # 임베드가 있는 경우에만
                 discord_user = await self.fetch_user(user_data['discord_uid'])  # Discord API를 통해 디스코드 유저를 가져옴
                 if discord_user:  # 유저가 있는 경우에만
-                    # 유저에게 메시지를 보냄
-                    await discord_user.send("\n\n".join(messages))
+                    for embed in embeds:
+                        await discord_user.send(embed=embed)  # 각 임베드를 유저에게 전송
                 else:
                     # 유저를 찾을 수 없는 경우 경고 로그를 남김
                     logger.warning(f"Could not fetch Discord user for user ID {user.id}")
             else:
                 # 핫딜이 없는 경우 로그를 남김
                 logger.info(f"No hot deals found for user {user.username}")
+
+
+
+    # async def send_hotdeal_alerts(self):
+    #     # 비동기 쿼리를 실행할 수 있도록 비동기 매니저를 사용
+    #     async_manager = AsyncManager()
+
+    #     # 활성화된 DiscordMessage 객체들을 가져옴. user와 keyword를 포함해 가져옴.
+    #     active_discord_messages = await async_manager.async_all(
+    #         DiscordMessage.objects.filter(active=True).select_related('user', 'keyword')
+    #     )
+
+    #     # 유저별로 DiscordMessage 객체를 그룹화
+    #     user_messages = {}  # 빈 딕셔너리를 생성하여 각 유저별로 메시지를 저장할 공간을 만듬
+    #     for discord_message in active_discord_messages:  # 모든 활성화된 DiscordMessage 객체를 순회
+    #         user = discord_message.user  # 각 메시지에서 유저 객체를 가져옴
+    #         if user not in user_messages:  # 유저가 딕셔너리에 없으면
+    #             user_messages[user] = {'discord_uid': discord_message.discord_uid, 'keywords': []}  # 유저를 키로 하고 discord_uid와 키워드 리스트를 값으로 추가
+    #         user_messages[user]['keywords'].append(discord_message.keyword.text)  # 유저의 리스트에 키워드를 추가
+
+    #     for user, user_data in user_messages.items():  # 그룹화된 유저별 메시지들을 순회
+    #         keywords = user_data['keywords']
+    #         messages = []  # 유저별로 보낼 메시지들을 저장할 리스트
+
+    #         for keyword in keywords:  # 각 유저의 키워드들을 순회
+    #             # 키워드에 해당하는 핫딜들을 가져옴
+    #             hotdeals = await async_manager.async_all(
+    #                 ScrappingModel.objects.filter(title__icontains=keyword, active=True)
+    #             )
+    #             if hotdeals:  # 핫딜이 있는 경우에만
+    #                 keyword_message = f"'{keyword}' 검색 결과:\n" + "\n".join(
+    #                     [f"{hotdeal.title}: {hotdeal.url}" for hotdeal in hotdeals]
+    #                 )
+    #                 messages.append(keyword_message)  # 메시지 리스트에 추가
+
+    #         if messages:  # 메시지가 있는 경우에만
+    #             discord_user = await self.fetch_user(user_data['discord_uid'])  # Discord API를 통해 디스코드 유저를 가져옴
+    #             if discord_user:  # 유저가 있는 경우에만
+    #                 # 유저에게 메시지를 보냄
+    #                 await discord_user.send("\n\n".join(messages))
+    #             else:
+    #                 # 유저를 찾을 수 없는 경우 경고 로그를 남김
+    #                 logger.warning(f"Could not fetch Discord user for user ID {user.id}")
+    #         else:
+    #             # 핫딜이 없는 경우 로그를 남김
+    #             logger.info(f"No hot deals found for user {user.username}")
 
 
 
