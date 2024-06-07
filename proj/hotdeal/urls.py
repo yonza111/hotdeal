67c0eef14c1897aed08db945c15b163fb719770b
yongja <kyj00001@gmail.com>
2024-06-07 12:23:10 +0900

    nondrfviews backup urls

--- proj/hotdeal/urls.py
+++ proj/hotdeal/urls.py
@@ -14,4 +14,13 @@ urlpatterns = [
     path('list/<int:pk>/', ScrappingDetailView.as_view(), name='detail'),
     path('category/<path:category>/', CategoryListView.as_view(), name='category_list'),
     path('search/', ScrappingSearchListView.as_view(), name='search'),
-    ]
+]
+
+
+# urlpatterns = [
+#     path('', main, name='main'),
+#     path('list/', ScrappingListView.as_view(), name='list'),
+#     path('list/<int:pk>/', ScrappingDetailView.as_view(), name='detail'),
+#     path('category/<path:category>/', CategoryListView.as_view(), name='category_list'),
+#     path('search/', ScrappingSearchListView.as_view(), name='search'),
+#     ]
