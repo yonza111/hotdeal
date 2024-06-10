f701ce0569a05dc30c2ca3c115ae8c8b0bf328ce
yongja <kyj00001@gmail.com>
2024-06-10 21:29:01 +0900

    make url easy to read by adding api in url

--- proj/hotdeal/urls.py
+++ proj/hotdeal/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from .views import (ScrappingListView, 
-                    main, 
+                    index, 
                     ScrappingDetailView, 
                     CategoryListView, 
                     ScrappingSearchListView,) 
@@ -9,11 +9,11 @@ app_name = "hotdeal"
 
 
 urlpatterns = [
-    path('', main, name='main'),
-    path('list/', ScrappingListView.as_view(), name='list'),
-    path('list/<int:pk>/', ScrappingDetailView.as_view(), name='detail'),
-    path('category/<path:category>/', CategoryListView.as_view(), name='category_list'),
-    path('search/', ScrappingSearchListView.as_view(), name='search'),
+    path('', index, name='index'),
+    path('api/scrappinglist/', ScrappingListView.as_view(), name='list'),
+    path('api/scrappinglist/<int:pk>/', ScrappingDetailView.as_view(), name='detail'),
+    path('api/category/<path:category>/', CategoryListView.as_view(), name='category_list'),
+    path('api/search/', ScrappingSearchListView.as_view(), name='search'),
 ]
 
 
