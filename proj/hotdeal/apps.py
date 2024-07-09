df2239d44d26cc236c4426ea10c8cbd955539e9a
yongja <kyj00001@gmail.com>
2024-07-09 14:13:02 +0900

    remove some comments

--- proj/hotdeal/apps.py
+++ proj/hotdeal/apps.py
@@ -9,12 +9,3 @@ logger = logging.getLogger(__name__)
 class HotdealConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hotdeal'
-    # # initialized = False  # 초기화 여부를 확인하는 플래그
-
-    # def ready(self):
-    #     # if not self.initialized:  # 초기화되지 않았을 때만 실행
-    #         # if settings.SCHEDULER_DEFAULT:
-    #             from .tasks import start
-    #             start()
-    #             logger.info("Scheduler started")
-    #         # self.initialized = True  # 초기화 완료 후 플래그 설정
